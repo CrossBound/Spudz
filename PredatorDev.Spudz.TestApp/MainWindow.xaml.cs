811c9702551c180b170e8ab1190718affdb54706
PSCapng <dtcomputerservice@gmail.com>
2016-03-04 00:11:07 -0600

    add wpf application for testing and add code for logging output

---
+++ PredatorDev.Spudz.TestApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PredatorDev.Spudz;
+
+namespace PredatorDev.Spudz.TestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        private StringLog _logger;
+        private FogBugzManager fogbugz;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // REFERENCE CREDIT: mijacobs @ https://msdn.microsoft.com/en-us/windows/uwp/controls-and-patterns/resourcedictionary-and-xaml-resource-references
+            _logger = (StringLog)this.Resources["LogData"];
+            _logger.AppendLine("initialized");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.fogbugz = new FogBugzManager("https://predatordev.fogbugz.com/api.asp", "9v8fvhlohlthu45bl7sodeius29ii2");
+                //this.fogbugz = new FogBugzManager("https://predatordev.fogbugz.com/api.asp", "test", "spassword");
+                _logger.AppendLine("Sign on successful");
+            }
+            catch (Exception error)
+            {
+                _logger.AppendLine(error.Message);
+            }
+        }
+    }
+}
---
+++ PredatorDev.Spudz.TestApp/StringLog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+
+namespace PredatorDev.Spudz.TestApp
+{
+    public class StringLog : INotifyPropertyChanged
+    {
+        /*
+            REFERENCE CREDITS: 
+            https://msdn.microsoft.com/en-us/library/ms743643(v=vs.100).aspx#classes
+            https://msdn.microsoft.com/en-us/library/ms743695(v=vs.100).aspx
+        */
+        private StringBuilder _builder = new StringBuilder();
+        public string Content => _builder.ToString();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Append(string Text)
+        {
+            _builder.Insert(0, Text);
+            OnPropertyChanged("Content");
+        }
+
+        public void AppendLine(string Text)
+        {
+            _builder.Insert(0, Environment.NewLine);
+            _builder.Insert(0, Text);
+            OnPropertyChanged("Content");
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
